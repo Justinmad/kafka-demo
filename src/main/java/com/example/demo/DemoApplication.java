2cfd4241e464ae2e4a0d5d65a8f2242a8847abdf
zhangyangyang <zhangyangyang@kaike.la>
2018-07-10 17:02:35 +0800

    spring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,45 @@
 package com.example.demo;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+    public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args).close();
     }
+
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    private final CountDownLatch latch = new CountDownLatch(3);
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.template.send("myTopic", "foo1");
+        this.template.send("myTopic", "foo2");
+        this.template.send("myTopic", "foo3");
+        latch.await(60, TimeUnit.SECONDS);
+        logger.info("All received");
+    }
+
+    @KafkaListener(topics = "myTopic")
+    public void listen(ConsumerRecord<?, ?> cr) {
+        logger.info(cr.toString());
+        latch.countDown();
+    }
+
 }
